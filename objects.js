15950188457df210fc5f92342b4c43b5f2fdac6e
Akhtar <reachtoakhtar@gmail.com>
2020-01-06 11:13:58 +0530

    Update objects.js, add .gitignore file.

--- objects.js
+++ objects.js
@@ -3,62 +3,67 @@ const circle = {
    radius: 1, 
    draw: function() {}
 }; 
-   
-// To create multiple objects with the same structure and behaviuor (methods), use a factory or a constructor. 
 
-// Factory function 
-function createCircle(radius) { 
+// To create multiple objects with the same structure and behaviuor (methods),
+// use a factory or a constructor.
+
+// Factory function
+function createCircle(radius) {
    return {
-      radius, 
+      radius,
       draw: function() {}
-   } 
-} 
+   }
+}
+const crl = createCircle(1);
 
-// Constructor function 
-function Circle(radius) { 
-    this.radius = radius; 
+// Constructor function
+function Circle(radius) {
+    this.radius = radius;
     this.draw = function() {}
-} 
-    
-// Every object has a "constructor" property which returns the function that was used to construct or create that object. 
+}
+const another = new Circle(1)
+
+
+// Every object has a "constructor" property which returns the function
+// that was used to construct or create that object.
 const x = {};
-x.constructor; // returns Object() 
-   
-// In JavaScript, functions are objects. They have properties and methods. 
-Circle.name; 
+x.constructor; // returns Object()
+
+// In JavaScript, functions are objects. They have properties and methods.
+Circle.name;
 Circle.length;
 Circle.constructor; // returns Function()
-Circle.call({}, 1); // to call the Circle function 
-Circle.apply({}, [1]);
+Circle.call({}, 1); // to call the Circle function
+Circle.apply({}, [1, 2]);
 
-// Value types are copied by their value, reference types are copied by their reference. 
+// Value types are copied by their value, reference types are copied by their reference.
 // Value types in JavaScript are: String, Number, Boolean, Symbol, undefined and null
-// Reference types are: Object, Function and Array 
-   
-// JavaScript objects are dynamic. You can add/remove properties: 
+// Reference types are: Object, Function and Array
+
+// JavaScript objects are dynamic. You can add/remove properties:
 circle.location = {};
 circle['location'] = {};
-                      
-delete circle.location; 
-                      
-// To enumerate the members in an object: 
+
+delete circle.location;
+
+// To enumerate the members in an object:
 for (let key in circle) console.log(key, circle[key]);
 
-Object.keys(circle); 
-                      
+const keys = Object.keys(circle);
+
 // To see if an object has a given property
-if ('location' in circle)
-                      
-// Abstraction means hiding the complexity/details and showing only the essentials. 
-// We can hide the details by using private members. Replace "this" with "let". 
+if ('location' in circle) console.log("location is in circle.")
 
-function Circle(radius) { 
-   // Public member 
-   this.radius = radius; 
+// Abstraction means hiding the complexity/details and showing only the essentials.
+// We can hide the details by using private members. Replace "this" with "let".
 
-   // Private member                       
-   let defaultLocation = {};                      
-}                       
+function Circle(radius) {
+   // Public member
+   this.radius = radius;
+
+   // Private member
+   let defaultLocation = {};
+}
 
 // To define a getter/setter, use Object.defineProperty():
 
