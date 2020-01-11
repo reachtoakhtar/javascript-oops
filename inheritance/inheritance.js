ea9d9b382f824bf7276bd14442bfa42761ba58c2
Akhtar <reachtoakhtar@gmail.com>
2020-01-11 20:58:13 +0530

    Update inheritance.js.

--- inheritance/inheritance.js
+++ inheritance/inheritance.js
@@ -1,44 +1,75 @@
+// Intermediate function inheritance
+function extend(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
 
-function Shape() {}
-function Circle() {}
-
-// Prototypical inheritance 
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle; 
-
-function Rectangle(color) {
-    // To call the super constructor 
-    Shape.call(this, color);
 }
 
-// Method overriding 
-Shape.prototype.draw = function() {}
+function Shape(color) {
+  this.color = color
+}
+
+Shape.prototype.duplicate = function () {
+  console.log("duplicate");
+}
+
+function Circle(radius, color) {
+  Shape.call(this, color)
+  this.radius = radius
+}
+
+// Prototypical inheritance
+// Circle.prototype = Object.create(Shape.prototype);
+// Circle.prototype.constructor = Circle;
+extend(Circle, Shape)
+
+// Method overriding
+Circle.prototype.duplicate = function() {
+  Shape.prototype.duplicate.call(this)
+  console.log("overridden circle duplicate");
+}
+
 Circle.prototype.draw = function() {
-    // Call the base implementation 
-    Shape.prototype.draw.call(this);
-
-    // Do additional stuff here 
+  console.log("draw");
 }
 
-// Don't create large inheritance hierarchies. 
-// One level of inheritance is fine. 
+function Square(size) {
+  this.size = size
+}
 
-// Use mixins to combine multiple objects 
-// and implement composition in JavaScript. 
-const canEat = { 
-    eat: function() {}
+extend(Square, Shape)
+
+Square.prototype.duplicate = function() {
+  console.log("overridden square duplicate");
+}
+
+const s = new Shape()
+const c = new Circle(1, "red")
+const sq = new Square(10)
+
+
+// Don't create large inheritance hierarchies. One level of inheritance is fine.
+// Use mixins to combine multiple objects and implement composition in JavaScript.
+
+const canEat = {
+  eat: function() {
+    console.log("canEat");
+  }
 };
 
 const canWalk = {
-    walk: function() {}
+  walk: function() {
+    console.log("canWalk");
+  }
 };
 
 function mixin(target, ...sources) {
-    // Copies all the properties from all the source objects 
-    // to the target object. 
-    Object.assign(target, ...sources);
+  // Copies all the properties from all the source objects to the target object.
+  Object.assign(target, ...sources);
 }
 
 function Person() {}
 
 mixin(Person.prototype, canEat, canWalk);
+
+const person = new Person()
